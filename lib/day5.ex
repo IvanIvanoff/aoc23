b010d0ce3860dae65b652eaad9bf13ee6ae44316
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-12-05 15:23:42 +0200

    Faster brute force Day 5

--- lib/day5.ex
+++ lib/day5.ex
@@ -1,82 +1,83 @@
 defmodule Day5 do
   def get_input(), do: File.read!("./lib/inputs/day5")
-  @cache :cached_locations
-  defp start_clean_ets() do
-    if :ets.whereis(@cache) != :undefined do
-      :ets.delete(@cache)
-    end
-
-    :ets.new(@cache, [:set, :named_table, :public])
-  end
 
   # Part A
   def run_a(input \\ get_input()) do
-    start_clean_ets()
+    lines = input |> String.split("\n")
 
-    input
-    |> String.split("\n")
-    |> parse_lines()
-    |> find_locations()
-    |> Enum.min_by(fn {_seed, location} -> location end)
-    |> elem(1)
+    ["seeds: " <> seeds | rest] = lines
+    seeds = parse_seeds_a(seeds)
+    map = rest |> parse_map()
+
+    find_min_location_with_seed(map, seeds)
   end
 
   # Part B
 
   def run_b(input \\ get_input()) do
-    start_clean_ets()
+    lines = input |> String.split("\n")
 
-    {map, seeds} =
-      input
-      |> String.split("\n")
-      |> parse_lines()
+    ["seeds: " <> seeds | rest] = lines
+    seeds = parse_seeds_b(seeds)
+    map = rest |> parse_map()
 
-    ranges =
-      seeds_ranges_to_values(seeds)
-
-    ranges
-    |> Task.async_stream(fn seeds_list -> find_locations({map, seeds_list}) end,
-      max_concurrency: System.schedulers_online()
-    )
-    |> Enum.map(fn {:ok, l} -> Enum.min_by(l, fn {_seed, location} -> location end) end)
-    |> List.flatten()
-    |> Enum.min_by(fn {_seed, location} -> location end)
+    find_min_location_with_seed(map, seeds)
   end
 
   # Private
 
-  defp seeds_ranges_to_values(seeds) do
+  def find_min_location_with_seed(map, seeds) do
+    keys_chain = keys_chain(map, "seed") |> Enum.reverse()
+    do_find_min_location_with_seed(map, seeds, keys_chain, 0)
+  end
+
+  def do_find_min_location_with_seed(map, seeds, keys_chain, location) do
+    seed = location_to_seed(map, keys_chain, location)
+
+    if Enum.any?(seeds, fn range -> seed in range end),
+      do: {seed, location},
+      else: do_find_min_location_with_seed(map, seeds, keys_chain, location + 1)
+  end
+
+  def seeds_ranges_to_values(seeds) do
     Enum.chunk_every(seeds, 2)
     |> Enum.map(fn [start, len] -> start..(start + len) |> Enum.to_list() end)
   end
 
-  defp parse_lines(lines) do
-    ["seeds: " <> seeds | rest] = lines
-
-    seeds_to_be_planted =
-      seeds |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
-
-    rest = Enum.chunk_by(rest, &(&1 == "")) |> Enum.reject(&(&1 == [""]))
-
-    map =
-      Enum.reduce(rest, %{}, fn [map_str | values], acc ->
-        # the key looks like ["seed", "soil"]
-        map_name = String.trim_trailing(map_str, " map:") |> String.split("-to-", trim: true)
-
-        values =
-          Enum.reduce(values, %{}, fn numbers, inner_acc ->
-            [a, b, c] = String.split(numbers, " ", trim: true) |> Enum.map(&String.to_integer/1)
-
-            Map.put(inner_acc, a..(a + c), b..(b + c))
-          end)
-
-        Map.merge(acc, %{map_name => values})
-      end)
-
-    {map, seeds_to_be_planted}
+  def parse_seeds_a(seeds) do
+    seeds
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&Range.new(&1, &1))
   end
 
-  defp keys_chain(map, "seed") do
+  def parse_seeds_b(seeds) do
+    seeds
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [start, len] -> Range.new(start, start + len) end)
+  end
+
+  def parse_map(lines) do
+    lines = Enum.chunk_by(lines, &(&1 == "")) |> Enum.reject(&(&1 == [""]))
+
+    Enum.reduce(lines, %{}, fn [map_str | values], acc ->
+      # the key looks like ["seed", "soil"]
+      map_name = String.trim_trailing(map_str, " map:") |> String.split("-to-", trim: true)
+
+      values =
+        Enum.reduce(values, %{}, fn numbers, inner_acc ->
+          [a, b, c] = String.split(numbers, " ", trim: true) |> Enum.map(&String.to_integer/1)
+
+          Map.put(inner_acc, a..(a + c - 1), b..(b + c - 1))
+        end)
+
+      Map.merge(acc, %{map_name => values})
+    end)
+  end
+
+  def keys_chain(map, "seed") do
     # Build the list of keys in the order that need to be traversed
     map_keys = Map.keys(map)
 
@@ -88,35 +89,14 @@ defmodule Day5 do
     |> Enum.chunk_every(2, 1, :discard)
   end
 
-  defp find_locations({map, seeds}) do
-    keys_chain = keys_chain(map, "seed")
+  def location_to_seed(map, keys_chain, location) do
+    Enum.reduce(keys_chain, location, fn key, current_value ->
+      list = map[key]
 
-    Enum.map(seeds, fn seed ->
-      {seed, seed_to_location(map, keys_chain, seed)}
+      case Enum.find(list, fn {dest_range, _source} -> current_value in dest_range end) do
+        nil -> current_value
+        {d1.._d2, s1.._s2} -> current_value + s1 - d1
+      end
     end)
-  end
-
-  defp seed_to_location(map, keys_chain, seed) do
-    case :ets.lookup(@cache, seed) do
-      [] ->
-        location =
-          Enum.reduce(keys_chain, seed, fn key, current_value ->
-            list = map[key]
-
-            case Enum.find(list, fn {_dest_range, source_range} ->
-                   current_value in source_range
-                 end) do
-              nil -> current_value
-              {d1.._d2, s1.._s2} -> current_value - s1 + d1
-            end
-          end)
-
-        :ets.insert(@cache, {seed, location})
-
-        location
-
-      [{_seed, location}] ->
-        location
-    end
   end
 end
