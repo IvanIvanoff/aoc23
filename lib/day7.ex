dafb066d65ad39fb3f26a8ccd1f61886c5f1ffc5
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-12-07 13:16:21 +0200

    Add day 7, part 2

--- lib/day7.ex
+++ lib/day7.ex
@@ -1,14 +1,12 @@
 defmodule Day7 do
-  @card_to_value ~w(2 3 4 5 6 7 8 9 T J Q K A) |> Enum.with_index(2) |> Map.new()
+  @card_to_value ~w(X 2 3 4 5 6 7 8 9 T J Q K A) |> Enum.with_index(1) |> Map.new()
   def get_input(), do: File.read!("./lib/inputs/day7")
 
   # Part A
   def run_a(input \\ get_input()) do
     input
     |> Utils.split_lines()
-    |> parse_eval_sort()
-    |> Enum.with_index(1)
-    |> Enum.reduce(0, fn {[_hand_value, bid], rank}, acc -> acc + bid * rank end)
+    |> eval()
   end
 
   # Part B
@@ -16,19 +14,38 @@ defmodule Day7 do
   def run_b(input \\ get_input()) do
     input
     |> Utils.split_lines()
+    |> Enum.map(&String.replace(&1, "J", "X"))
+    |> eval()
   end
 
-  defp parse_eval_sort(lines) do
+  defp eval(list) do
+    list |> parse() |> Enum.map(fn {hand, bid} -> {hand_value(hand), bid} end) |> sort_sum()
+  end
+
+  defp parse(lines) do
     Enum.map(lines, fn line ->
-      [cards, bid] = String.split(line, " ", trim: true)
-      [hand_value(cards), String.to_integer(bid)]
+      [hand, bid] = String.split(line, " ", trim: true)
+      {String.split(hand, "", trim: true), String.to_integer(bid)}
     end)
-    |> Enum.sort_by(fn [hand_value, _bid] -> hand_value end, :asc)
   end
 
-  defp hand_value(hand) do
-    cards = String.split(hand, "", trim: true)
-    frequencies = cards |> Enum.frequencies() |> Map.values() |> Enum.sort(:desc)
-    {frequencies, Enum.map(cards, &@card_to_value[&1])}
+  defp sort_sum(list) do
+    list
+    |> Enum.sort_by(fn {hand_value, _bid} -> hand_value end, :asc)
+    |> Enum.with_index(1)
+    |> Enum.reduce(0, fn {{_hand_value, bid}, rank}, acc -> acc + bid * rank end)
+  end
+
+  defp hand_value(cards) do
+    frequencies = cards |> Enum.frequencies()
+    {jokers, rest} = Map.pop(frequencies, "X", 0)
+
+    vals =
+      case rest |> Map.values() |> Enum.sort(:desc) do
+        [] -> [jokers]
+        [top | rest] -> [top + jokers | rest]
+      end
+
+    {vals, Enum.map(cards, &@card_to_value[&1])}
   end
 end
