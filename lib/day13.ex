e995d0bcba87bc194024ce38c6d477751bd7f6bb
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-12-15 01:11:37 +0200

    Add day 13, part b

--- lib/day13.ex
+++ lib/day13.ex
@@ -3,26 +3,27 @@ defmodule Day13 do
 
   # Part A
   def run_a() do
-    input = get_input()
+    count_mirrors(get_input(), :part_a)
+  end
 
+  # Part B
+  def run_b() do
+    count_mirrors(get_input(), :part_b)
+  end
+
+  defp count_mirrors(input, part) do
     for section <- input do
-      row_mirrors = section |> find_mirrors()
-      col_mirrors = section |> Nx.transpose() |> find_mirrors()
+      row_mirrors = section |> find_mirrors(part)
+      col_mirrors = section |> Nx.transpose() |> find_mirrors(part)
 
       Enum.sum(row_mirrors) * 100 + Enum.sum(col_mirrors)
     end
     |> Enum.sum()
   end
 
-  # Part B
-
-  def run_b() do
-    get_input()
-  end
-
-  defp find_mirrors(tensor) do
+  defp find_mirrors(tensor, part) do
     {r, _c} = Nx.shape(tensor)
-    for i <- 1..(r - 1), mirror?(tensor, i), do: i
+    for i <- 1..(r - 1), mirror?(tensor, i, part), do: i
   end
 
   defp parse(input) do
@@ -42,13 +43,20 @@ defmodule Day13 do
     end)
   end
 
-  defp mirror?(tensor, i) do
+  defp mirror?(tensor, i, part) do
     {r, _c} = Nx.shape(tensor)
     size = Enum.min([i, r - i])
 
     above = tensor[(i - size)..(i - 1)]
     below = tensor[i..(i + size - 1)] |> Nx.reverse(axes: [0])
 
-    above == below
+    case part do
+      :part_a ->
+        above == below
+
+      :part_b ->
+        smudges = Nx.subtract(above, below) |> Nx.abs() |> Nx.sum()
+        Nx.to_number(smudges) == 1
+    end
   end
 end
