c1093a151c24fd945b293319fb3c9f45ec93db01
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-12-18 13:42:58 +0200

    bakcup

--- lib/day18.ex
+++ lib/day18.ex
@@ -3,9 +3,8 @@ defmodule Day18 do
 
   def run_a() do
     get_input()
-    # |> get_path_points()
-    # |> path_area()
     |> get_edges()
+    |> IO.inspect(label: "7", limit: :infinity)
     |> area()
   end
 
@@ -21,56 +20,84 @@ defmodule Day18 do
     end)
   end
 
-  defp area(path) do
-    Enum.map(path, fn {x, y} -> )
+  def area(edges) do
+    Enum.chunk_every(edges, 2, 1, :discard)
+    |> Enum.map(fn [{x1, y1}, {x2, y2}] -> x1 * y2 - x2 * y1 end)
+    |> Enum.sum()
+    |> abs()
+    |> Kernel./(2)
   end
 
   defp get_edges(instructions) do
     Enum.reduce(instructions, [{0, 0}], fn {dir, steps, _color}, [curr | _] = acc ->
-      [move(curr, dir)] | acc]
-
-    end)
-  end
-
-  defp get_path_points(instructions) do
-    Enum.reduce(instructions, [{0, 0}], fn {dir, steps, _color}, acc ->
-      Enum.reduce(1..steps, acc, fn _, [curr | _] = inner_acc -> [move(curr, dir) | inner_acc] end)
+      [move(curr, dir, steps) | acc]
     end)
     |> Enum.reverse()
+    |> print()
   end
 
-  defp path_area(path) do
-    {x_l, _} = Enum.min_by(path, &elem(&1, 0))
-    {x_r, _} = Enum.max_by(path, &elem(&1, 0))
-    {_, y_l} = Enum.min_by(path, &elem(&1, 1))
-    {_, y_r} = Enum.max_by(path, &elem(&1, 1))
+  # defp get_path_points(instructions) do
+  #   Enum.reduce(instructions, [{0, 0}], fn {dir, steps, _color}, acc ->
+  #     Enum.reduce(1..steps, acc, fn _, [curr | _] = inner_acc ->
+  #       [move(curr, dir, 1) | inner_acc]
+  #     end)
+  #   end)
+  #   |> Enum.reverse()
+  # end
 
-    in_or_on_path =
-      for r <- x_l..x_r, c <- y_l..y_r, {r, c} in path or inside_dig?({r, c}, path) do
-        {r, c}
+  # defp path_area(path) do
+  #   {x_l, _} = Enum.min_by(path, &elem(&1, 0))
+  #   {x_r, _} = Enum.max_by(path, &elem(&1, 0))
+  #   {_, y_l} = Enum.min_by(path, &elem(&1, 1))
+  #   {_, y_r} = Enum.max_by(path, &elem(&1, 1))
+
+  #   in_or_on_path =
+  #     for r <- x_l..x_r, c <- y_l..y_r, {r, c} in path or inside_dig?({r, c}, path) do
+  #       {r, c}
+  #     end
+
+  #   length(in_or_on_path)
+  # end
+
+  # defp inside_dig?({x, y}, path) do
+  #   row_left_points = Enum.filter(path, fn {r, c} -> r == x and c < y end)
+
+  #   inversions =
+  #     Enum.count(row_left_points, fn {r, c} ->
+  #       {r + 1, c} in path
+  #     end)
+
+  #   rem(inversions, 2) == 1
+  # end
+
+  defp move({x, y}, dir, step) do
+    case dir do
+      "D" -> {x + step, y}
+      "U" -> {x - step, y}
+      "L" -> {x, y - step}
+      "R" -> {x, y + step}
+    end
+  end
+
+  defp print(edges) do
+    {x_l, _} = Enum.min_by(edges, &elem(&1, 0))
+    {x_r, _} = Enum.max_by(edges, &elem(&1, 0))
+    {_, y_l} = Enum.min_by(edges, &elem(&1, 1))
+    {_, y_r} = Enum.max_by(edges, &elem(&1, 1))
+
+    for x <- x_l..x_r do
+      for y <- y_l..y_r do
+        if {x, y} in edges do
+          IO.write("X")
+        else
+          IO.write(".")
+        end
       end
 
-    length(in_or_on_path)
-  end
-
-  defp inside_dig?({x, y}, path) do
-    row_left_points = Enum.filter(path, fn {r, c} -> r == x and c < y end)
-
-    inversions =
-      Enum.count(row_left_points, fn {r, c} ->
-        {r + 1, c} in path
-      end)
-
-    rem(inversions, 2) == 1
-  end
-
-  defp move({x, y}, dir) do
-    case dir do
-      "D" -> {x, y + 1}
-      "U" -> {x, y - 1}
-      "L" -> {x - 1, y}
-      "R" -> {x + 1, y}
+      IO.write("\n")
     end
+
+    edges
   end
 end
 
